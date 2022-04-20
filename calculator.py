654780d8532c52e29735bb118b8cdadc3cf11b6e
Mythkiller776 <89923496+Mythkiller776@users.noreply.github.com>
2022-04-20 19:51:43 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = int(input(' Enter the first number ' ))
+b = int(input(' Enter the second number '))
+
+sum = a + b
+difference = a - b
+product = a*b
+quotient = a/b
+remainder = a%b
+
+print ('sum is :  ',sum)
+print ('difference is : ',difference)
+print ('product is : ',product)
+print ('quotient is: ',quotient)
+print ('remainder is : ',remainder)
